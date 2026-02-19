6eb7532de17f0f7b1c05d441e25278de1c42eb8e
diamankayero <yero.diamanka@hotmail.com>
2026-02-19 03:23:26 +0100

    Supprime build et dist du dépôt

--- build/lib/trainedml/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Permet d'importer facilement les modèles depuis le sous-package models.
-"""
-from .base import BaseModel
-from .knn import KNNModel
-from .logistic import LogisticModel
-from .random_forest import RandomForestModel
--- build/lib/trainedml/viz/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ce fichier permet d'importer le sous-package viz
-from .vizs import Vizs
-from .heatmap import HeatmapViz
-from .histogram import HistogramViz
-from .line import LineViz
